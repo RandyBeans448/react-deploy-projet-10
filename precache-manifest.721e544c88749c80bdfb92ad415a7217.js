440559d60620abbcd56caea1035bc1ab8af7589b
RandyBeans448 <dmncbishop@gmail.com>
2020-12-15 20:10:20 +0000

    Updates

---
+++ precache-manifest.721e544c88749c80bdfb92ad415a7217.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1571b4746e3c5d0aad153e29f6381ee9",
+    "url": "/react-deploy-projet-10/index.html"
+  },
+  {
+    "revision": "b8d22d41397b5244da88",
+    "url": "/react-deploy-projet-10/static/css/main.3d70f792.chunk.css"
+  },
+  {
+    "revision": "7ba762cad9ad83deb6bf",
+    "url": "/react-deploy-projet-10/static/js/2.08096025.chunk.js"
+  },
+  {
+    "revision": "a88bb54fe1e397f3cd18139de199ef5d",
+    "url": "/react-deploy-projet-10/static/js/2.08096025.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8d22d41397b5244da88",
+    "url": "/react-deploy-projet-10/static/js/main.e68741c8.chunk.js"
+  },
+  {
+    "revision": "ed622b4122a525b40ba9",
+    "url": "/react-deploy-projet-10/static/js/runtime-main.035e92a1.js"
+  }
+]);
